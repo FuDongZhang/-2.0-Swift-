e4682e3057ead170b600cb47a099a9d48c23fb5c
FuDongZhang <1341490669@qq.com>
2017-08-16 18:39:13 +0800

    2017 08-16 18:40

---
+++ InstalmenHelpSwift/Classes/homePage(首页)/Views/IHHomeThirdCell.swift
@@ -0,0 +1,24 @@
+//
+//  IHHomeThirdCell.swift
+//  InstalmenHelpSwift
+//
+//  Created by XQT-zfd on 2017/8/16.
+//  Copyright © 2017年 XQT-zfd. All rights reserved.
+//
+
+import UIKit
+
+class IHHomeThirdCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
