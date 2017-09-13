b4942f68633ecbb199ae29376780e42e32dc04ea
FuDongZhang <1341490669@qq.com>
2017-09-13 15:14:58 +0800

    09-1

---
+++ InstalmenHelpSwift/Classes/QRCode(二维码)/Views/IHQRCodeCell.swift
@@ -0,0 +1,24 @@
+//
+//  IHQRCodeCell.swift
+//  InstalmenHelpSwift
+//
+//  Created by XQT-zfd on 2017/9/11.
+//  Copyright © 2017年 XQT-zfd. All rights reserved.
+//
+
+import UIKit
+
+class IHQRCodeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
